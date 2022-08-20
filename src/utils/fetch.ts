fcf858e044cf18d1ee90850c5f9f7fb108f00794
Saurabh Ghosh <kkooldandy@gmail.com>
2022-08-21 00:48:17 +0530

    Fixed build issue

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -4,15 +4,14 @@ import getApiEndpoints from "./api-endpoints";
 const fetchData = async (apiName: ApiNameEnum) => {
   let data = null;
   const url = getApiEndpoints(apiName);
-  const headers = { 'Content-Type': 'application/json' };
+  const headers = { "Content-Type": "application/json" };
   try {
-    const res = await fetch(url,{headers});
+    const res = await fetch(url, { headers });
     data = await res.json();
   } catch (err) {
-    console.error('err', err);
-  } finally {
-    return data;
+    console.error("err", err);
   }
+  return data;
 };
 
-export {fetchData};
+export { fetchData };
