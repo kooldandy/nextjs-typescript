e9fd0e39d98bc3c8a6738f6da5e7c3e7697e4e0d
Saurabh Ghosh <kkooldandy@gmail.com>
2022-08-21 11:55:28 +0530

    Fixed 404 page issue

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,20 @@
-export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+import React from "react";
+
+const Custom404 = (props: {message}) => {
+  return <h1>{props.message}</h1>;
+};
+
+Custom404.getTitle = () => {
+  return "Error 404";
+};
+
+export async function getStaticProps() {
+
+  const data = {message: '404 - Oops Page Not Found'}
+
+  return {
+    props: {props: data}
   }
+}
+
+export default Custom404;
